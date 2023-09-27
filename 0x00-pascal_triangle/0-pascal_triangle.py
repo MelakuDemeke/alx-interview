ca6205e0e4330a0501ab602531353382e1f7a638
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-27 22:27:57 +0300

    define empty list to represent pascal triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''A module for working with Pascal's triangle.
+'''
+
+
+def pascal_triangle(n):
+    '''Creates an empty list to represent Pascal's triangle.
+    '''
+    return []
