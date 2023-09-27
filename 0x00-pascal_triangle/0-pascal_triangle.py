528e7c4903fb4807d83c77b34c1e28a3f496cd23
MelakuDemeke <melaku.demeke789@gmail.com>
2023-09-27 22:31:11 +0300

    generate additional rows

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,10 +5,20 @@
 
 def pascal_triangle(n):
     '''Checks if n is a positive integer and
-        creates the first row of Pascal's triangle.
+        generates additional rows of Pascal's triangle.
     '''
     if not isinstance(n, int) or n <= 0:
         return []
 
     triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+
+        row.append(1)
+        triangle.append(row)
+
     return triangle
