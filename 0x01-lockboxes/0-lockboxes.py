cd112566dacd4a3a538c17e6b56e6b86c09d2d9c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-04 15:16:20 +0300

    add doc describing the module

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,2 +1,9 @@
+#!/usr/bin/python3
+""" Lockboxes 
+
+This module provides a function for checking if all the boxes in a list
+of lockboxes can be unlocked, given that the first box is unlocked.
+"""
+
 def canUnlockAll(boxes):
   pass
