532982ce050cef125839aa6061816eff81199070
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-04 15:18:13 +0300

    add odc describe the canUnlockAll function

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-""" Lockboxes 
+""" Lockboxes
 
 This module provides a function for checking if all the boxes in a list
 of lockboxes can be unlocked, given that the first box is unlocked.
 """
 
+
 def canUnlockAll(boxes):
-  pass
+    '''
+    Checks if all the lockboxes can be unlocked.
+
+    Args:
+        boxes (list of lists): A list of lockboxes where each box contains
+                              keys (indices) to other boxes.
+
+    Returns:
+        bool: True if all lockboxes can be unlocked, False otherwise.
+    '''
+    pass
