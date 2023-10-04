b4591bdbe534c54a1b593b73701824abe440a4b0
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-04 15:20:41 +0300

    add the logic and return

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -21,4 +21,15 @@ def canUnlockAll(boxes):
     seen_boxes = set([0])
     unseen_boxes = set(boxes[0]).difference(set([0]))
 
-    
+    while len(unseen_boxes) > 0:
+        box_index = unseen_boxes.pop()
+
+        # Ensure box_index is within valid bounds
+        if not (0 <= box_index < num_boxes):
+            continue
+
+        if box_index not in seen_boxes:
+            unseen_boxes = unseen_boxes.union(boxes[box_index])
+            seen_boxes.add(box_index)
+
+    return num_boxes == len(seen_boxes)
