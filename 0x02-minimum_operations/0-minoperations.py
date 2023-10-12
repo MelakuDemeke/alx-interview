53f117bbeb3a9615e0f281247039a35adcffd2e7
Melaku <MelakuDemeke>
2023-10-12 08:11:34 +0300

    add doc string

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
-
+'''
+This script defines a function to compute the fewest number of operations
+needed to result in exactly n 'H' characters. It provides an efficient
+approach to achieving this by simulating copy and paste operations.
+'''
 def minOperations(n):
+    ''''
+    Computes the fewest number of operations needed to result
+    in exactly n 'H' characters.
+
+    :param n: The target number of 'H' characters.
+    :return: The minimum number of operations required.
+    '''
     if not isinstance(n, int) or n <= 0:
         return 0
 
