265f8def9900ed2bf55599c406921ce302e6c077
Melaku <MelakuDemeke>
2023-10-12 08:12:37 +0300

    fix pycode style

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,6 +4,8 @@ This script defines a function to compute the fewest number of operations
 needed to result in exactly n 'H' characters. It provides an efficient
 approach to achieving this by simulating copy and paste operations.
 '''
+
+
 def minOperations(n):
     ''''
     Computes the fewest number of operations needed to result
