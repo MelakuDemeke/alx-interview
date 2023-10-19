59c9562f34670a2643ed35e8cc3563844f713b7a
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-19 16:26:49 +0300

    define usefull vars

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,1 +1,6 @@
 #!/usr/bin/python3
+
+status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
+                      '403': 0, '404': 0, '405': 0, '500': 0}
+total_file_size = 0
+line_count = 0
