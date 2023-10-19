1add91fa6a2d6b8c9e943351701d96b800c0a037
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-19 16:35:24 +0300

    embed in try catch

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -7,18 +7,22 @@ status_code_counts = {'200': 0, '301': 0, '400': 0, '401': 0,
 total_file_size = 0
 line_count = 0
 
-for line in sys.stdin:
-    parts = line.split(" ")
-    if len(parts) > 4:
-        http_status_code = parts[-2]
-        file_size = parts[-1]
-        if http_status_code in status_code_counts:
-                status_code_counts[http_status_code] += 1
-        total_file_size += file_size
-        line_count += 1
-    if line_count == 10:
-            line_count = 0
-            print('Total File Size: {}'.format(total_file_size))
-            for code, count in sorted(status_code_counts.items()):
-                if count != 0:
-                    print('{}: {}'.format(code, count))
+try:
+    for line in sys.stdin:
+        parts = line.split(" ")
+        if len(parts) > 4:
+            http_status_code = parts[-2]
+            file_size = parts[-1]
+            if http_status_code in status_code_counts:
+                    status_code_counts[http_status_code] += 1
+            total_file_size += file_size
+            line_count += 1
+        if line_count == 10:
+                line_count = 0
+                print('Total File Size: {}'.format(total_file_size))
+                for code, count in sorted(status_code_counts.items()):
+                    if count != 0:
+                        print('{}: {}'.format(code, count))
+except Exception as error:
+    pass
+
