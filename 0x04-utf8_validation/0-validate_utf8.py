9295b53a411e564ecc082e7831e6d2987dc402e3
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 15:33:15 +0300

    start processing byte data and chek if itis10xxxxx

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -3,3 +3,11 @@
 def validUTF8(data):
     # Number of bytes in the current UTF-8 character
     num_bytes = 0
+
+    for byte in data:
+        # Check if the current byte is a continuation byte (10xxxxxx)
+        if 128 <= byte <= 191:
+            if num_bytes == 0:
+                return False
+            num_bytes -= 1
+        
