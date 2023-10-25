bd1f591f82ae3925e57f99097dfed272411b9021
MelakuDemeke <melaku.demeke789@gmail.com>
2023-10-25 15:41:11 +0300

    chnage full logic

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,28 +10,23 @@ def validUTF8(data):
     :return: True if data is a valid UTF-8 encoding, else return False.
     '''
     # Number of bytes in the current UTF-8 character
-    num_bytes = 0
+    bytes_to_expect = 0
 
     for byte in data:
-        # Check if the current byte is a continuation byte (10xxxxxx)
-        if 128 <= byte <= 191:
-            if num_bytes == 0:
-                return False
-            num_bytes -= 1
-        else:
-            # Check the number of bytes needed to represent this character
-            if num_bytes > 0:
-                return False
-            if 1 <= byte <= 127:
-                num_bytes = 0
-            elif 192 <= byte <= 223:
-                num_bytes = 1
-            elif 224 <= byte <= 239:
-                num_bytes = 2
-            elif 240 <= byte <= 247:
-                num_bytes = 3
+        if bytes_to_expect == 0:
+            if byte < 128:
+                continue
+            elif byte < 224:
+                bytes_to_expect = 1
+            elif byte < 240:
+                bytes_to_expect = 2
+            elif byte < 248:
+                bytes_to_expect = 3
             else:
                 return False
+        else:
+            if byte < 128 or byte >= 192:
+                return False
+            bytes_to_expect -= 1
 
-    # Check if there are any remaining bytes to complete a character
-    return num_bytes == 0
+    return bytes_to_expect == 0
