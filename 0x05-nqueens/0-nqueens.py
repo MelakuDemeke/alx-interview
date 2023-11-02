aacba4a6ffc44a00d6d4e0943be274650b3bbcbd
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-02 10:41:00 +0300

    nqueen solver

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,7 +2,23 @@
 import sys
 
 def is_safe(board, row, col, N):
+    """
+    Check if it is safe to place a queen at a given position on the board.
+
+    Args:
+    - board: list of integers representing the current state of the board
+    - row: integer representing the row to check
+    - col: integer representing the column to check
+    - N: integer representing the size of the board
+
+    Returns:
+    - True if it is safe to place a queen at the given position, False otherwise
+    """
     for i in range(row):
         if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
             return False
     return True
+
+
+def solve_nqueens(N):
+    pass
