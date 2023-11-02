b8f589359f98c085d4d7eebbc821008ec719055f
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-02 10:46:14 +0300

    pycode style fix

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def is_safe(board, row, col, N):
     """
     Check if it is safe to place a queen at a given position on the board.
@@ -12,10 +13,14 @@ def is_safe(board, row, col, N):
     - N: integer representing the size of the board
 
     Returns:
-    - True if it is safe to place a queen at the given position, False otherwise
+    - True if it is safe to place a queen at the given position False otherwise
     """
     for i in range(row):
-        if board[i] == col or board[i] - i == col - row or board[i] + i == col + row:
+        if (
+            board[i] == col
+            or board[i] - i == col - row
+            or board[i] + i == col + row
+        ):
             return False
     return True
 
@@ -43,6 +48,7 @@ def solve_nqueens(N):
                 board[row] = -1
 
     solve(0)
+
 
 if len(sys.argv) != 2:
     print("Usage: nqueens N")
