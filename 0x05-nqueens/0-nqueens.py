6abf8bfb57e5cd6ce04d0903ef0f6e319738d6d2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-02 10:57:32 +0300

    reverse

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,6 +2,39 @@
 '''N Queens Problem'''
 import sys
 
+
+def is_safe(board, row, col, N):
+    for i in range(row):
+        if (
+            board[i] == col
+            or board[i] - i == col - row
+            or board[i] + i == col + row
+        ):
+            return False
+    return True
+
+
+def solve_nqueens(N):
+    board = [-1] * N
+
+    def print_solution(board):
+        solution = [[i, col] for i, col in enumerate(board)]
+        print(solution)
+
+    def solve(row):
+        if row == N:
+            print_solution(board)
+            return
+
+        for col in range(N):
+            if is_safe(board, row, col, N):
+                board[row] = col
+                solve(row + 1)
+                board[row] = -1
+
+    solve(0)
+
+
 if __name__ == '__main__':
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
@@ -17,31 +50,4 @@ if __name__ == '__main__':
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [-1] * N
-
-    def is_safe(row, col):
-        for i in range(row):
-            if (
-                board[i] == col
-                or board[i] - i == col - row
-                or board[i] + i == col + row
-            ):
-                return False
-        return True
-
-    def print_solution():
-        solution = [[i, col] for i, col in enumerate(board)]
-        print(solution)
-
-    def solve(row):
-        if row == N:
-            print_solution()
-            return
-
-        for col in range(N):
-            if is_safe(row, col):
-                board[row] = col
-                solve(row + 1)
-                board[row] = -1
-
-    solve(0)
+    solve_nqueens(N)
