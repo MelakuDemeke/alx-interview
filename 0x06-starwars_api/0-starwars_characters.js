cc262ff0c2874d10c33875d0a27b73c063e67da1
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 10:23:29 +0300

    define the move path

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const swapiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+function fetchMovieCharacters() {
+}
+
+fetchMovieCharacters();
