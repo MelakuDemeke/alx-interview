a4375e83f5bfd14e3f0e885bd828d5abd504762b
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-08 10:27:00 +0300

    semi standard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -5,31 +5,31 @@ const movieId = process.argv[2];
 
 const swapiUrl = `https://swapi.dev/api/films/${movieId}/`;
 
-function fetchMovieCharacters() {
-    request(swapiUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-            const movieData = JSON.parse(body);
-            const characters = movieData.characters;
+function fetchMovieCharacters () {
+  request(swapiUrl, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const movieData = JSON.parse(body);
+      const characters = movieData.characters;
 
-            function printCharacterNames(index) {
-                if (index < characters.length) {
-                    request(characters[index], (error, response, body) => {
-                        if (!error && response.statusCode === 200) {
-                            const characterData = JSON.parse(body);
-                            console.log(characterData.name);
-                            printCharacterNames(index + 1);
-                        } else {
-                            console.error(error);
-                        }
-                    });
-                }
+      function printCharacterNames (index) {
+        if (index < characters.length) {
+          request(characters[index], (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+              const characterData = JSON.parse(body);
+              console.log(characterData.name);
+              printCharacterNames(index + 1);
+            } else {
+              console.error(error);
             }
-
-            printCharacterNames(0);
-        } else {
-            console.error(error);
+          });
         }
-    });
+      }
+
+      printCharacterNames(0);
+    } else {
+      console.error(error);
+    }
+  });
 }
 
 fetchMovieCharacters();
