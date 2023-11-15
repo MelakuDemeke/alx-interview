9f26e44c0f413004f7b7384aecd06e3ecb75918c
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-15 11:36:04 +0300

    Add function to rotate 2D matrix 90 degrees clockwise

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,4 +4,10 @@
 
 
 def rotate_2d_matrix(matrix):
-    pass
+    """ Given an n x n 2D matrix, rotate it 90 degrees clockwise.
+    """
+    if not all(isinstance(row, list) for row in matrix) or not matrix:
+        return
+    rows, cols = len(matrix), len(matrix[0])
+    matrix[:] = [[matrix[r][c] for r in range(rows - 1, -1, -1)]
+                 for c in range(cols)]
