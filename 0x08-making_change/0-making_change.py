db5e29787310dfa8a8044a16f3999a670b59a091
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-21 10:59:13 +0300

    add doc

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,6 +4,9 @@
 
 
 def makeChange(coins, total):
+    """Given a pile of coins of different values, determine the fewest number
+    of coins needed to meet a given amount total.
+    """
     if total <= 0:
         return 0
     dp = [float('inf')] * (total + 1)
