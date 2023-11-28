bdbded943786d52f6b930e1e52a87524262bf604
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 12:42:37 +0300

    Refactor island_perimeter function to skip cells with value 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -9,15 +9,8 @@ def island_perimeter(grid):
     perimeter = 0
     n = len(grid)
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                perimeter += 4
-                # Check left neighbor
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-                # Check top neighbor
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-
-    return perimeter
+    for i, row in enumerate(grid):
+        m = len(row)
+        for j, cell in enumerate(row):
+            if cell == 0:
+                continue
