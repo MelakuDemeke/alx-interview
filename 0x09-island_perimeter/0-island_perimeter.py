ece6afc0cd29bb5448dc0662907255926bb2d428
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 12:36:04 +0300

    pycodestyle fix

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ Island Perimeter """
 
+
 def island_perimeter(grid):
     if not grid or not grid[0]:
         return 0
-    
+
     rows, cols = len(grid), len(grid[0])
     perimeter = 0
 
@@ -18,5 +19,5 @@ def island_perimeter(grid):
                 # Check top neighbor
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
-    
+
     return perimeter
