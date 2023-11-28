b9893108a1bd5eacda59e22c6522ea242ebc30c2
MelakuDemeke <melaku.demeke789@gmail.com>
2023-11-28 12:43:01 +0300

    Add island perimeter calculation logic

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -14,3 +14,9 @@ def island_perimeter(grid):
         for j, cell in enumerate(row):
             if cell == 0:
                 continue
+            edges = (
+                i == 0 or (len(grid[i - 1]) > j and grid[i - 1][j] == 0),
+                j == m - 1 or (m > j + 1 and row[j + 1] == 0),
+                i == n - 1 or (len(grid[i + 1]) > j and grid[i + 1][j] == 0),
+                j == 0 or row[j - 1] == 0,
+            )
